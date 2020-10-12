5a3a51a68b9f8a4b0c9e1d861da56158d87dcf39
Yamada Ryutaro <yamada@Yamada.local>
2020-10-12 09:02:53 +0900

    rev

--- js/main.js
+++ js/main.js
@@ -40,3 +40,18 @@ jQuery(function($){
       }
 	}
 });
+
+$(function(){
+  $(window).scroll(function (){
+      $('.fadein').each(function(){
+          var targetElement = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > targetElement - windowHeight + 200){
+              $(this).css('opacity','1');
+              $(this).css('transform','translateY(0)');
+          }
+      });
+  });
+  jQuery(window).scroll();
+});
